205b72c62d78a38a3322d86e62a69dd4d1233793
Ceri Stagg <ceristaggbusiness@gmail.com>
2015-09-12 15:13:11 -0700

    Initial Commit

---
+++ CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData
+//
+//  Created by Ceri Stagg on 2015-09-12.
+//  Copyright (c) 2015 Long Lourney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreData/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreData
+//
+//  Created by Ceri Stagg on 2015-09-12.
+//  Copyright (c) 2015 Long Lourney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
